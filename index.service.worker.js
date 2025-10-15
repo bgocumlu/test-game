d4d2298b77fa0c2f596d050c088e4976e0b2c437
bgocumlu <bgocumlu@users.noreply.github.com>
2025-10-15 22:56:59 +0300

    u

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759671039|2451964471';
+const CACHE_VERSION = '1760558187|3380592540';
 /** @type {string} */
 const CACHE_PREFIX = 'New Game Project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
